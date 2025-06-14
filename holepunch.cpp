a7691559a5331521e18b436383376e912313505a
something1010 <something1010@mail.ru>
2025-06-14 04:57:48 +0300

    Reformatted holepunch.cpp

--- holepunch.cpp
+++ holepunch.cpp
@@ -4,13 +4,39 @@
 
 using namespace std;
 
+string calculateMask(string inputm int density) {
+	string mask = "";
+
+	while (input.length() > 0) {
+		if (rand() % (density + 1) == 0) {
+			result += input[0];
+			input.erase(0, 1);
+			mask += ' ';
+		}
+		else {
+			result += letters[rand() % letters.length()];
+			mask += '@';
+		}
+	}
+
+	return mask
+}
+
+void display(string message) cout << message;
+
+string getString() {
+	string output = "";
+	cin >> output;
+	return output;
+}
+
 int main(){
 
 	srand(time(0));
 
 	string message = "lorem ipsum";
 
-	cin >> message;
+	message = getString();
 	
 	const string letters = "0123456789abcdefghijklmnopqrstuvwxyz ";
 	string mask = "";
@@ -19,24 +45,17 @@ int main(){
 	int density = 7;
 	//int totalCount = message.length() * density;
 	
-	while (message.length() > 0) {
-		if (rand() % (density + 1) == 0) {
-			result += message[0];
-			message.erase(0, 1);
-			mask += ' ';
-		} else {
-			result += letters[rand() % letters.length()];
-			mask += '@';
-		}
-	}
+	mask = calculateMask(message, density);
+	
 	int sideL = sqrt(result.length()) * 2;
 	for (int i = 1; i < result.length() + 1; i++) {
-		cout << result[i - 1] << ' ';
-		if (i % sideL == 0) cout << endl;
+		display(result[i - 1]);
+		if (i % sideL == 0) display("\n");
 	}
 	cout << endl;
 	for (int i = 1; i < result.length() + 1; i++) {
-		cout << mask[i - 1] << '@';
-		if (i % sideL == 0) cout << endl;
+		display(mask[i - 1]);
+		display("@");
+		if (i % sideL == 0) display("\n");
 	}
 }                                                                                                                                                                                                                                                                                             
