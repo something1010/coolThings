c32df3734134b9128617d33a69dff690a3f27270
something1010 <something1010@mail.ru>
2025-06-14 04:59:03 +0300

    Fixed bugs in holepuch.cpp

--- holepunch.cpp
+++ holepunch.cpp
@@ -4,25 +4,43 @@
 
 using namespace std;
 
-string calculateMask(string inputm int density) {
-	string mask = "";
+string calculateResult(string input, int density, string charactrs) {
+	string mask = "", finalMask = "";
+	string message = "", finalMessage = "";
+	string temp = input;
 
-	while (input.length() > 0) {
+	while (temp.length() > 0) {
 		if (rand() % (density + 1) == 0) {
-			result += input[0];
-			input.erase(0, 1);
+			message += temp[0];
+			temp.erase(0, 1);
 			mask += ' ';
 		}
 		else {
-			result += letters[rand() % letters.length()];
+			message += charactrs[rand() % charactrs.length()];
 			mask += '@';
 		}
 	}
 
-	return mask
+	for (int i = 0; i < message.length() * 2; i++) {
+		if (i % 2 == 0) {
+			finalMessage += message[i / 2];
+			finalMask += mask[i / 2];
+		} else {
+			finalMessage += charactrs[rand() % charactrs.length()];
+			finalMask += "@";
+		}
+	}
+
+	return message + "\n" + mask;
 }
 
-void display(string message) cout << message;
+void display(string message) {
+	cout << message; 
+}
+
+void display(char message) {
+	cout << message;
+}
 
 string getString() {
 	string output = "";
@@ -38,24 +56,31 @@ int main(){
 
 	message = getString();
 	
+	message.reserve();
+
 	const string letters = "0123456789abcdefghijklmnopqrstuvwxyz ";
 	string mask = "";
 	string result = "";
-
+	string output2;
+	int temp = 0;
 	int density = 7;
-	//int totalCount = message.length() * density;
 	
-	mask = calculateMask(message, density);
-	
-	int sideL = sqrt(result.length()) * 2;
+	string output = calculateResult(message, density, letters);
+	output2 = output;
+
+	result = output.erase(output.find_first_of('\n', 0), output.length());
+	mask = output2.erase(0, output2.find_first_of('\n', 0) + 1);
+
+	int sideL = sqrt((output.length() - 1) / 2 ) * 2;
+
 	for (int i = 1; i < result.length() + 1; i++) {
 		display(result[i - 1]);
 		if (i % sideL == 0) display("\n");
 	}
-	cout << endl;
+	display('\n');
+
 	for (int i = 1; i < result.length() + 1; i++) {
 		display(mask[i - 1]);
-		display("@");
 		if (i % sideL == 0) display("\n");
 	}
 }                                                                                                                                                                                                                                                                                             
